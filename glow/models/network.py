66ed7f2dc4b863c238fd3c00864308c767a32256
bhavya <bhavyabhatt17@gmail.com>
2019-07-09 20:02:08 +0530

    updated version files

---
+++ glow/models/network.py
@@ -0,0 +1,87 @@
+from torch import nn
+import torch
+from glow.utils import Losses as L
+from glow.utils import Optimizers as O
+
+
+class Network(nn.Module):
+    """
+    Keras Sequential type implementation for
+    PyTorch
+
+    """
+    def __init__(self, input_dim):
+        super().__init__()
+        self.input_dim = input_dim  # input dimensions
+        self.layer_list = nn.ModuleList([])  # list of module type layers
+        self.num_layers = 0  # number of layers in the architecture
+        if(torch.cuda.is_available()):
+            print("running on cuda enabled GPU")
+
+    def add(self, layer_obj):
+        if(self.num_layers == 0):
+            prev_input_dim = self.input_dim
+        else:
+            prev_input_dim = self.layer_list[self.num_layers-1][-1].output_dim
+        layer_obj.set_input(prev_input_dim)
+        self.layer_list.append(self._make_layer_unit(layer_obj))
+        self.num_layers = self.num_layers + 1
+
+    def _make_layer_unit(self, layer_obj):
+        layers = []
+        layers.append(layer_obj)
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        layers = self.layer_list
+        h = x
+        iter_num = 0
+        for layer in layers:
+            h = layer(h)
+            iter_num += 1
+        return h
+
+    def compile(self, optimizer_name='SGD', loss='cross_entropy', learning_rate=0.001, momentum=0.95):
+        self.criterion = L.loss_function(loss)
+        self.optimizer = O.optimizer(self.parameters(), learning_rate, momentum, optimizer_name)
+
+    def fit(self, x_train, y_train, batch_size=1, num_epochs=50, num_classes=10):
+        print("starting training process")
+        for epoch in range(num_epochs):
+            running_loss = 0.0
+            for index, x in enumerate(x_train):
+                target = y_train[index]
+                self.optimizer.zero_grad()
+                output = self.forward(x)
+                loss = self.criterion(output, target)
+                loss.backward()
+                self.optimizer.step()
+                running_loss += loss.item()
+                if index % 2000 == 1999:    # print every 2000 mini-batches
+                    print('[%d, %5d] loss: %.3f' %
+                          (epoch + 1, index + 1, running_loss / 2000))
+                    running_loss = 0.0
+
+        print("^^^^^^^^^^^^^^^^^")
+        print("Training finished !")
+
+    def fit_generator(self, trainloader, batch_size=64, num_epochs=5, num_classes=10):
+        for e in range(num_epochs):
+            running_loss = 0
+            for images, labels in trainloader:
+                # Flatten MNIST images into a 784 long vector
+                images = images.view(images.shape[0], -1)
+                # TODO: Training pass
+                self.optimizer.zero_grad()
+                output = self.forward(images)
+                loss = self.criterion(output, labels)
+                loss.backward()
+                self.optimizer.step()
+                running_loss += loss.item()
+            else:
+                print(f"Training loss: {running_loss/len(trainloader)}")
+
+    def predict(self, x):
+        return self.forward(x)
+
+
