9c920150d0a2487268abab86eb3baac6dbed86c1
Ritwik Saha <s.ritwik98@gmail.com>
2019-07-10 10:38:25 +0530

    configure package structure

--- setup.py
+++ setup.py
@@ -1,18 +1,8 @@
-import setuptools
+#!/usr/bin/env python
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from setuptools import setup
 
-setuptools.setup(
-    name="PyGlow",
-    version="0.0.1",
-    author="Bhavya",
-    author_email="bhavyabhatt17@gmail.com",
-    description="This package is an attempt to implement Keras like API functionalities on PyTorch backend.",
-    long_description=long_description,
-    url="https://github.com/spino17/PyGlow",
-    install_requires=['numpy'],
-    license='MIT',
-    # We will also need a readme eventually (there will be a warning)
-    # long_description=open('README.txt').read(),
-)
+# https://packaging.python.org/guides/single-sourcing-package-version/
+# http://blog.ionelmc.ro/2014/05/25/python-packaging/
+
+setup(setup_cfg=True)
---
+++ src/glow/tests/test_initiate.py
@@ -0,0 +1,2 @@
+def test_initiate():
+    assert True
