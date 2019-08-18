e050d266d391527f04ebefe381e6e38c30795127
bhavya <bhavyabhatt17@gmail.com>
2019-08-19 00:24:34 +0530

    empty functions added in normalization module

--- src/glow/layers/normalization.py
+++ src/glow/layers/normalization.py
@@ -7,14 +7,44 @@ class _BatchNorm(nn.Module):
         super(_BatchNorm, self).__init__()
         # TODO
 
+    def set_input(self, input_shape):
+        # TODO
+
+    def forward(self, x):
+        # TODO
+
 
 class BatchNorm1d(_BatchNorm):
-    # TODO
+    def __init__(self, ):
+        super().__init__()
+        # TODO
+
+    def set_input(self, input_shape):
+        super().set_input(input_shape)
+
+    def forward(self, x):
+        return super().forward(x)
 
 
 class BatchNorm2d(_BatchNorm):
-    # TODO
+    def __init__(self, ):
+        super().__init__()
+        # TODO
+
+    def set_input(self, input_shape):
+        super().set_input(input_shape)
+
+    def forward(self, x):
+        return super().forward(x)
 
 
 class BatchNorm3d(_BatchNorm):
-    # TODO
+    def __init__(self, ):
+        super().__init__()
+        # TODO
+
+    def set_input(self, input_shape):
+        super().set_input(input_shape)
+
+    def forward(self, x):
+        return super().forward(x)
