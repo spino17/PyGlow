67b16def11cd8fc8abf5d09d10a43440d4b587cf
bhavya <bhavyabhatt17@gmail.com>
2019-08-27 12:27:14 +0530

    all bugs fixed in packaging and is available on test PyPI as name PyGlow 0.0.6

--- glow/__init__.py
+++ glow/__init__.py
@@ -5,20 +5,10 @@ PyGlow
 
 """
 
-"""
-from __future__ import absolute_import
-
-from . import information_bottelneck
-from . import architechures
-from . import datasets
-from . import layers
-from . import models
-from . import preprocessing
-from . import utils
-"""
+__all__ =  ['information_bottleneck', 'architechures', 'datasets', 'layers', 'models', 'preprocessing', 'utils']
 
 from . import coordinates
 from . import metrics
 from . import tensor_numpy_adapter
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
