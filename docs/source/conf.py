b21ed4507b02d8c80bc069eb56bfb60da979927d
GaganDeep Tomar <gagandeep280597@gmail.com>
2019-09-12 13:03:18 +0530

    Added RTD theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,16 +13,16 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("./"))
-sys.path.append(os.path.abspath("./../../glow/models/"))
-sys.path.append(os.path.abspath("./../../glow/models/network.py"))
-sys.path.append(os.path.abspath("./../../glow/models/hsic.py"))
-sys.path.append(os.path.abspath("./../../glow/layers/"))
-sys.path.append(os.path.abspath("./../../glow/information_bottleneck/"))
-sys.path.append(os.path.abspath("./../../glow/preprocessing/"))
-sys.path.append(os.path.abspath("./../../glow/datasets/"))
-sys.path.append(os.path.abspath("./../../glow/architechures/"))
-sys.path.append(os.path.abspath("./../../glow/"))
+sys.path.insert(0, os.path.abspath("/home/gdt/code/PyGlow/docs/source"))
+sys.path.append(os.path.abspath("/home/gdt/code/PyGlow/glow/models/"))
+sys.path.append(os.path.abspath("/home/gdt/code/PyGlow/glow/models/network.py"))
+sys.path.append(os.path.abspath("/home/gdt/code/PyGlow/glow/models/hsic.py"))
+sys.path.append(os.path.abspath("/home/gdt/code/PyGlow/glow/layers/"))
+sys.path.append(os.path.abspath("/home/gdt/code/PyGlow/glow/information_bottleneck/"))
+sys.path.append(os.path.abspath("/home/gdt/code/PyGlow/glow/preprocessing/"))
+sys.path.append(os.path.abspath("/home/gdt/code/PyGlow/glow/datasets/"))
+sys.path.append(os.path.abspath("/home/gdt/code/PyGlow/glow/architechures/"))
+sys.path.append(os.path.abspath("/home/gdt/code/PyGlow/glow/"))
 
 # -- Project information -----------------------------------------------------
 
@@ -49,15 +49,18 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+autodoc_mock_imports = ['torch', 'glow']
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_static_path = ["_static"]
+
+
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
